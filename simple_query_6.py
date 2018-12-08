16058b832db1aef9247c63d9195f20b095b0c4b4
nelsondude <alexn1336@gmail.com>
2018-12-07 22:15:17 -0500

    Change to __main__

--- simple_query_6.py
+++ simple_query_6.py
@@ -23,7 +23,6 @@ def view_profile(username):
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    #return rows[0][0]
 
 def update_profile(new_username, old_username):
     query = '''
@@ -33,14 +32,10 @@ def update_profile(new_username, old_username):
          '''
     cmd = cur.mogrify(query, [new_username, old_username])
     cur.execute(query, [new_username, old_username ])
-    #rows = cur.fetchall()
-    #return rows[0][0]
 
-def main():
+if __name__ == "__main__":
     username = 'AnnieS'
     new_username = 'ASpahn'
     view_profile(username)
     update_profile(new_username,username)
     view_profile(new_username)
-
-main()
