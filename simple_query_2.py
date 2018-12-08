76da090f41a84093ba400096c27bbd034478ec43
Max Kornyev <maxkornyev@yahoo.com>
2018-12-07 22:00:18 -0500

    Simple 2 change

--- simple_query_2.py
+++ simple_query_2.py
@@ -3,15 +3,32 @@ See venmo withdrawals and deposits
 
 """
 
+
 import psycopg2 as pg2
 
 con = pg2.connect(database='venmo', user='isdb')
 con.autocommit = True
 cur = con.cursor()
 
+print("After Annie transfers 1000 to her venmo account, and Max overdraws")
 
-print('Story 2: see all venmo withdrawals and transfers')
 query = '''
+UPDATE BankAccounts
+SET balance = (balance - 1000)
+WHERE account_number = 1000377241; 
+
+UPDATE Users
+SET venmo_balance = (venmo_balance + 1000)
+WHERE u_id = 5;
+
+UPDATE BankAccounts
+SET balance = (balance - 4000)
+WHERE account_number = 1592784900; 
+
+UPDATE Users
+SET venmo_balance = (venmo_balance + 4000)
+WHERE u_id = 3;
+
 SELECT abs(amount),
        CASE
          WHEN amount > 0 THEN 1
@@ -23,8 +40,5 @@ SELECT abs(amount),
 '''
 cur.execute(query)
 rows = cur.fetchall()
-if rows:
-    for row in rows:
-        print(row)
-else:
-    print('No transfers found')
+for row in rows:
+    print(row)
